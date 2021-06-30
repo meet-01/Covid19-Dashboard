1ed6b919773fa32d7fc447f3771213157ff1d682
Meet Master <meetmaster16@gmail.com>
2021-06-30 13:45:54 +0530

    auth

---
+++ src/Layout/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container } from "reactstrap";
+
+export default function Footer() {
+  return (
+    <Container
+      fluid
+      tag="footer"
+      className=" foot text-center text-white   text-uppercase fixed-bottom"
+    >
+      Covid 19 Dashboard
+    </Container>
+  );
+}
